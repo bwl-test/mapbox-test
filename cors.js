ecbfea0e4bd49f397581678d9b546c6925f45dae
白文磊 <baiwenlei@sogou-inc.com>
2020-03-13 12:43:03 +0800

    跨域访问服务启动脚本

---
+++ cors.js
@@ -0,0 +1,16 @@
+// Create our server
+var server;
+server = http.createServer(function(req,res){
+	// Set CORS headers
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	if ( req.method === 'OPTIONS' ) {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
+
+	// ...
+});
